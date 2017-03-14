6c658b584e73009b0c3029410e1efb187ad440ca
Alexandra Tooley <sandratooley@rocketmail.com>
2017-03-13 20:26:27 -0600

    petshop

--- pets.js
+++ pets.js
@@ -1,60 +1,65 @@
-'use strict';
+'use strict'
 
-const fs = require('fs')
- const path = require('path')
+ var fs = require('fs')
+ var path = require('path')
  var petsPath = path.join(__dirname, 'pets.json');
 
- let node = path.basename(process.argv[0]);
- let file = path.basename(process.argv[1]);
- let cmd = process.argv[2];
+ var node = path.basename(process.argv[0]);
+ var file = path.basename(process.argv[1]);
+ var cmd = process.argv[2];
  var index = process.argv[3]
 
  if (cmd === 'read') {
-   var ind = process.argv[3]
-    fs.readFile('pets.json', 'utf8', function(err, data) {
+    fs.readFile(petsPath, 'utf8', function(err, data) {
         if (err) {
-          console.error(`USAGE: ${node} ${file} read`);
-        } else{
-          console.log('success', JSON.parse(data));
-          if (ind >= 0 && ind < JSON.parse(data).length) {
-            console.log(JSON.parse(data)[ind]);
-          } else if (ind !== undefined && typeof ind !== Number){
-            console.log(`USAGE: ${node} ${file} read INDEX`);
-          }
-          else {
-            console.log(JSON.parse(data))
-          }
+             throw err;
+         }
+         var pets = JSON.parse(data);
+
+         if (pets[index] === undefined) {
+             console.error(`Usage: ${node} ${file} read INDEX`)
         }
-      })
-    } else if (cmd === 'create'){
-      fs.readFile('pets.json', 'utf8', function (err, data){
-        if (err){
-          console.error(`USAGE: ${node} ${file}create AGE KIND NAME`);
-          process.exit(1);
-        }
-        var age = process.argv[3]
-         let kind = process.argv[4]
-          let name = process.argv[5]
-         let parsedData = JSON.parse(data)
-         let petObj = {}
-      if (!name){
-         console.error(`Usage: ${node} ${file} create AGE KIND NAME`);
-         process.exit(1)
- }else{
-   petObj.age = Number(age);
-   petObj.kind = kind;
-   petObj.name = name;
-   parsedData.push(petObj);
-   fs.writeFile('pets.json', JSON.stringify(parsedData), function(writingErr){
-     if (writingError){
-       throw writingError;
-     }
-     console.log((petObj));
-   })
+         else if (index) {
+             console.log(pets[index]);
+         } else {
+             console.log(pets);
+         }
+     });
+ } else if (cmd === 'create') {
+     fs.readFile(petsPath, 'utf8', function(readErr, data) {
+         if (readErr) {
+             throw readErr;
+         }
+
+         var pets = JSON.parse(data)
+         var age = process.argv[3]
+         var kind = process.argv[4]
+        var name = process.argv[5]
+
+         var pet = {
+             age: age,
+             kind: kind,
+             name: name
+         }
+
+         if (!age || !kind || !name) {
+             console.error(`Usage: ${node} ${file} ${cmd} AGE KIND NAME`);
+             process.exit(1);
+         }
+
+         pets.push(pet)
+
+         var petsJSON = JSON.stringify(pets)
+
+        fs.writeFile(petsPath, petsJSON, function(writeErr) {
+             if (writeErr) {
+                 throw writeErr
+            }
+
+             console.log(pet)
+         });
+     });
+ } else {
+     console.error(`Usage: ${node} ${file} [read | create | update | destroy]`)
+     process.exit(1)
  }
-      })
-    }
-    else {
-      console.error(`Usage: ${node} ${file} [read | create | update | destry]`);
-      process.exit(1);
-    }
