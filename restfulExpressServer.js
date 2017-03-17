463f654c3d62155bcb35ed281b89ade430dfe3a3
Alexandra Tooley <sandratooley@rocketmail.com>
2017-03-17 09:15:32 -0600

    petshopexpress

---
+++ restfulExpressServer.js
@@ -0,0 +1,33 @@
+'use strict'
+const fs = require('fs');
+const express = require('express');
+const morgan = require('morgan');
+app.use(morgan('short'));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 8000;
+let petsPath = path.join(__dirname, 'pets.json');
+
+ app.disable('x-powered-by');
+
+ app.post('/pets', function(req, res) {
+   var pet = req.body;
+
+   if (!pets) {
+     return res.sendStatus(400);
+   }
+
+   pets.push(pet);
+
+   res.send(pet);
+ });
+ app.get('/pets/:3', function(req, res) {
+  res.send(pets);
+});
+
+app.patch('/pets:index', function(req, res){
+  if
+})
